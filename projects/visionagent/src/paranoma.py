226dec2dee7e483168c3122d25ed0791d77c7ca9
juneon <junun24@gmail.com>
2024-11-13 19:08:47 +0900

    저장하면 네모 사라짐

--- projects/visionagent/src/paranoma.py
+++ projects/visionagent/src/paranoma.py
@@ -1,97 +1,142 @@
+from PyQt6.QtWidgets import *
 import cv2
 import numpy as np
-from PyQt6.QtWidgets import *
+import winsound
 import sys
 
-
-class SpecialEffect(QMainWindow):
+class Panorama(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("사진 특수 효과")
-        self.setGeometry(200, 200, 800, 200)
+        self.setWindowTitle("파노라마 영상")
+        self.setGeometry(200, 200, 700, 200)
 
-        pictureButton = QPushButton("사진 읽기", self)
-        embossButton = QPushButton("엠보싱", self)
-        cartoonButton = QPushButton("카툰", self)
-        sketchButton = QPushButton("연필 스케치", self)
-        oilButton = QPushButton("유화", self)
-        saveButton = QPushButton("저장하기", self)
-        self.pickCombo = QComboBox(self)
-        self.pickCombo.addItems(["엠보싱", "카툰", "연필 스케치(명암)", "연필 스케치(컬러)", "유화"])
+        collectButton = QPushButton("촬영", self)
+        self.stitchButton = QPushButton("파노라마", self)
+        self.textButton = QPushButton("글자 입력", self)
+        self.saveButton = QPushButton("저장", self)
         quitButton = QPushButton("나가기", self)
         self.label = QLabel("환영합니다!", self)
 
-        pictureButton.setGeometry(10, 10, 100, 30)
-        embossButton.setGeometry(110, 10, 100, 30)
-        cartoonButton.setGeometry(210, 10, 100, 30)
-        sketchButton.setGeometry(310, 10, 100, 30)
-        oilButton.setGeometry(410, 10, 100, 30)
-        saveButton.setGeometry(510, 10, 100, 30)
-        self.pickCombo.setGeometry(510, 40, 110, 30)
-        quitButton.setGeometry(620, 10, 100, 30)
-        self.label.setGeometry(10, 40, 500, 170)
+        collectButton.setGeometry(10, 25, 100, 30)
+        self.stitchButton.setGeometry(110, 25, 100, 30)
+        self.textButton.setGeometry(210, 25, 100, 30)
+        self.saveButton.setGeometry(310, 25, 100, 30)
+        quitButton.setGeometry(450, 25, 100, 30)
+        self.label.setGeometry(10, 70, 600, 170)
 
-        pictureButton.clicked.connect(self.pictureOpenFunction)
-        embossButton.clicked.connect(self.embossFunction)
-        cartoonButton.clicked.connect(self.cartoonFunction)
-        sketchButton.clicked.connect(self.sketchFunction)
-        oilButton.clicked.connect(self.oilFunction)
-        saveButton.clicked.connect(self.saveFunction)
+        self.stitchButton.setEnabled(False)
+        self.textButton.setEnabled(False)
+        self.saveButton.setEnabled(False)
+
+        collectButton.clicked.connect(self.collectFunction)
+        self.stitchButton.clicked.connect(self.stitchFunction)
+        self.textButton.clicked.connect(self.textFunction)
+        self.saveButton.clicked.connect(self.saveFunction)
         quitButton.clicked.connect(self.quitFunction)
 
-    def pictureOpenFunction(self):
-        fname = QFileDialog.getOpenFileName(self, "사진 읽기", "./data")
-        self.img = cv2.imread(fname[0])
-        if self.img is None:
-            sys.exit("파일을 찾을 수 없습니다.")
+        self.rect_start = None
+        self.rect_end = None
+        self.text_entered = False
 
-        cv2.imshow("Painting", self.img)
+    def collectFunction(self):
+        self.stitchButton.setEnabled(False)
+        self.textButton.setEnabled(False)
+        self.saveButton.setEnabled(False)
+        self.label.setText("c를 여러 번 눌러 수집하고 끝나면 q를 눌러 비디오를 끕니다.")
 
-    def embossFunction(self):
-        femboss = np.array([[-1.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
+        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        if not self.cap.isOpened():
+            sys.exit("카메라 연결 실패")
 
-        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        gray16 = np.int16(gray)
-        self.emboss = np.uint8(np.clip(cv2.filter2D(gray16, -1, femboss) + 128, 0, 255))
+        self.imgs = []
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                break
 
-        cv2.imshow("Emboss", self.emboss)
+            cv2.imshow("video display", frame)
 
-    def cartoonFunction(self):
-        self.cartoon = cv2.stylization(self.img, sigma_s=60, sigma_r=0.45)
-        cv2.imshow("Cartoon", self.cartoon)
+            key = cv2.waitKey(1)
+            if key == ord("c"):
+                self.imgs.append(frame)  
+            elif key == ord("q"):
+                self.cap.release()
+                cv2.destroyWindow("video display")
+                break
 
-    def sketchFunction(self):
-        self.sketch_gray, self.sketch_color = cv2.pencilSketch(
-            self.img, sigma_s=60, sigma_r=0.07, shade_factor=0.02
-        )
-        cv2.imshow("Pencil sketch(gray)", self.sketch_gray)
-        cv2.imshow("Pencil sketch(color)", self.sketch_color)
+        if len(self.imgs) >= 2: 
+            self.stitchButton.setEnabled(True)
+            self.saveButton.setEnabled(True)
 
-    def oilFunction(self):
-        self.oil = cv2.xphoto.oilPainting(self.img, 10, 1, cv2.COLOR_BGR2Lab)
-        cv2.imshow("Oil painting", self.oil)
+    def stitchFunction(self):
+        stitcher = cv2.Stitcher_create()
+        status, self.img_stitched = stitcher.stitch(self.imgs)
+        if status == cv2.STITCHER_OK:
+            self.img_display = self.img_stitched.copy()  
+            cv2.imshow("Image stitched panorama", self.img_display)
+            cv2.setMouseCallback("Image stitched panorama", self.draw)
+            self.textButton.setEnabled(True)
+        else:
+            winsound.Beep(3000, 500)
+            self.label.setText("파노라마 제작에 실패했습니다. 다시 시도하세요.")
+
+    def draw(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.rect_start = (x, y)
+        elif event == cv2.EVENT_RBUTTONDOWN and self.rect_start:
+            self.rect_end = (x, y)
+            self.img_display = self.img_stitched.copy()  
+            cv2.rectangle(self.img_display, self.rect_start, self.rect_end, (0, 0, 255), 2)
+            cv2.imshow("Image stitched panorama", self.img_display)
+            self.text_entered = False
+
+    def textFunction(self):
+        if not self.rect_start or not self.rect_end:
+            self.label.setText("사각형을 먼저 그려주세요")
+            return
+
+        rect_width = abs(self.rect_end[0] - self.rect_start[0])
+        rect_height = abs(self.rect_end[1] - self.rect_start[1])
+        rect_center_x = (self.rect_start[0] + self.rect_end[0]) // 2
+        rect_center_y = (self.rect_start[1] + self.rect_end[1]) // 2
+
+        text, ok = QInputDialog.getText(self, "글자 입력", "사각형에 넣을 글자를 입력하세요:")
+        if ok and text:
+            font_scale = 1
+            while True:
+                (text_width, text_height), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, font_scale, 2)
+                if text_width > rect_width or text_height > rect_height:
+                    font_scale -= 0.1
+                    break
+                font_scale += 0.1
+
+            self.text = text
+            self.font_scale = max(0.1, font_scale)
+            self.text_width, self.text_height = text_width, text_height
+            self.rect_center_x, self.rect_center_y = rect_center_x, rect_center_y
+
+            text_x = rect_center_x - text_width // 2
+            text_y = rect_center_y + text_height // 2
+
+            
+            cv2.putText(self.img_display, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, self.font_scale, (0, 255, 0), 2)
+            cv2.putText(self.img_stitched, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, self.font_scale, (0, 255, 0), 2)
+            cv2.imshow("Image stitched panorama", self.img_display)
+            self.text_entered = True
 
     def saveFunction(self):
-        fname = QFileDialog.getSaveFileName(self, "파일 저장", "./")
+        fname = QFileDialog.getSaveFileName(self, "파일 저장", "./data")
+        if fname[0]:
+            cv2.imwrite(fname[0], self.img_stitched)
 
-        i = self.pickCombo.currentIndex()
-        if i == 0:
-            cv2.imwrite(fname[0], self.emboss)
-        elif i == 1:
-            cv2.imwrite(fname[0], self.cartoon)
-        elif i == 2:
-            cv2.imwrite(fname[0], self.sketch_gray)
-        elif i == 3:
-            cv2.imwrite(fname[0], self.sketch_color)
-        elif i == 4:
-            cv2.imwrite(fname[0], self.oil)
 
     def quitFunction(self):
+        if hasattr(self, 'cap'):
+            self.cap.release()
         cv2.destroyAllWindows()
         self.close()
 
-
 app = QApplication(sys.argv)
-win = SpecialEffect()
+win = Panorama()
 win.show()
 app.exec()
--- projects/visionagent/src/special_effect.py
+++ projects/visionagent/src/special_effect.py
@@ -1,58 +1,97 @@
-import cv2 as cv
+import cv2
 import numpy as np
 from PyQt6.QtWidgets import *
 import sys
 
-class VideoSpecialEffect(QMainWindow):
+
+class SpecialEffect(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('비디오 특수 효과')
-        self.setGeometry(200,200,400,100)
+        self.setWindowTitle("사진 특수 효과")
+        self.setGeometry(200, 200, 800, 200)
 
-        videoButton=QPushButton('비디오 시작', self)
-        self.pickCombo=QComboBox(self)
-        self.pickCombo.addItems(['엠보싱','카툰','연필 스케치(명암)', '연필 스케치(컬러)',' 유화'])
-        quitButton=QPushButton('나가기',self)
+        pictureButton = QPushButton("사진 읽기", self)
+        embossButton = QPushButton("엠보싱", self)
+        cartoonButton = QPushButton("카툰", self)
+        sketchButton = QPushButton("연필 스케치", self)
+        oilButton = QPushButton("유화", self)
+        saveButton = QPushButton("저장하기", self)
+        self.pickCombo = QComboBox(self)
+        self.pickCombo.addItems(["엠보싱", "카툰", "연필 스케치(명암)", "연필 스케치(컬러)", "유화"])
+        quitButton = QPushButton("나가기", self)
+        self.label = QLabel("환영합니다!", self)
 
-        videoButton.setGeometry(10,10,140,30)
-        self.pickCombo.setGeometry(150,10,110,30)
-        quitButton.setGeometry(280,10,100,30)
+        pictureButton.setGeometry(10, 10, 100, 30)
+        embossButton.setGeometry(110, 10, 100, 30)
+        cartoonButton.setGeometry(210, 10, 100, 30)
+        sketchButton.setGeometry(310, 10, 100, 30)
+        oilButton.setGeometry(410, 10, 100, 30)
+        saveButton.setGeometry(510, 10, 100, 30)
+        self.pickCombo.setGeometry(510, 40, 110, 30)
+        quitButton.setGeometry(620, 10, 100, 30)
+        self.label.setGeometry(10, 40, 500, 170)
 
-        videoButton.clicked.connect(self.videoSpecialEffectFunction)
+        pictureButton.clicked.connect(self.pictureOpenFunction)
+        embossButton.clicked.connect(self.embossFunction)
+        cartoonButton.clicked.connect(self.cartoonFunction)
+        sketchButton.clicked.connect(self.sketchFunction)
+        oilButton.clicked.connect(self.oilFunction)
+        saveButton.clicked.connect(self.saveFunction)
         quitButton.clicked.connect(self.quitFunction)
-    
-    def videoSpecialEffectFunction(self):
-        self.cap=cv.VideoCapture(0,cv.CAP_DSHOW)
-        if not self.cap.isOpened(): sys.exit('카메라 연결 실패')
 
-        while True:
-            ret,frame=self.cap.read()
-            if not ret: break
+    def pictureOpenFunction(self):
+        fname = QFileDialog.getOpenFileName(self, "사진 읽기", "./data")
+        self.img = cv2.imread(fname[0])
+        if self.img is None:
+            sys.exit("파일을 찾을 수 없습니다.")
 
-            pick_effect=self.pickCombo.currentIndex()
-            if pick_effect==0:
-                femboss=np.array([[-1.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,1.0]])
-                gray=cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-                gray16=np.int16(gray)
-                special_img=np.uint8(np.clip(cv.filter2D(gray16,-1,femboss)+128,0,255))
-            elif pick_effect==1:
-                special_img=cv.stylization(frame,sigma_s=60,sigma_r=0.45)
-            elif pick_effect==2:
-                special_img,_=cv.pencilSketch(frame,sigma_s=60,sigma_r=0.07,shade_factor=0.02)    
-            elif pick_effect==3:
-                _,special_img=cv.pencilSketch(frame,sigma_s=60,sigma_r=0.07, shade_factor=0.02) 
-            elif pick_effect==4:
-                special_img=cv.xphoto.oilPainting(frame,10,1,cv.COLOR_BGR2Lab) 
-            
-            cv.imshow('Special effect',special_img)
-            cv.waitKey(1)
-                                    
+        cv2.imshow("Painting", self.img)
+
+    def embossFunction(self):
+        femboss = np.array([[-1.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
+
+        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        gray16 = np.int16(gray)
+        self.emboss = np.uint8(np.clip(cv2.filter2D(gray16, -1, femboss) + 128, 0, 255))
+
+        cv2.imshow("Emboss", self.emboss)
+
+    def cartoonFunction(self):
+        self.cartoon = cv2.stylization(self.img, sigma_s=60, sigma_r=0.45)
+        cv2.imshow("Cartoon", self.cartoon)
+
+    def sketchFunction(self):
+        self.sketch_gray, self.sketch_color = cv2.pencilSketch(
+            self.img, sigma_s=60, sigma_r=0.07, shade_factor=0.02
+        )
+        cv2.imshow("Pencil sketch(gray)", self.sketch_gray)
+        cv2.imshow("Pencil sketch(color)", self.sketch_color)
+
+    def oilFunction(self):
+        self.oil = cv2.xphoto.oilPainting(self.img, 10, 1, cv2.COLOR_BGR2Lab)
+        cv2.imshow("Oil painting", self.oil)
+
+    def saveFunction(self):
+        fname = QFileDialog.getSaveFileName(self, "파일 저장", "./")
+
+        i = self.pickCombo.currentIndex()
+        if i == 0:
+            cv2.imwrite(fname[0], self.emboss)
+        elif i == 1:
+            cv2.imwrite(fname[0], self.cartoon)
+        elif i == 2:
+            cv2.imwrite(fname[0], self.sketch_gray)
+        elif i == 3:
+            cv2.imwrite(fname[0], self.sketch_color)
+        elif i == 4:
+            cv2.imwrite(fname[0], self.oil)
+
     def quitFunction(self):
-        self.cap.release()
-        cv.destroyAllWindows()
+        cv2.destroyAllWindows()
         self.close()
 
-app=QApplication(sys.argv)
-win=VideoSpecialEffect()
+
+app = QApplication(sys.argv)
+win = SpecialEffect()
 win.show()
 app.exec()
